0dfb1a83a9af1cff07bbe27d4f35c69cbcb66f05
KariWhite <katiwka.belova@icloud.com>
2026-03-01 22:49:02 +0200

    Final NewsPortal project with authorization

---
+++ protect/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'protect/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем проверку: НЕ является ли пользователь автором
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
+
+
+from django.shortcuts import render
+
+# Create your views here.
